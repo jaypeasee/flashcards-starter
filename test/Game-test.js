3edbba712d1b3086798dcf3f7c0b2d16e6c1f132
JP Carey <jpcarey4@gmail.com>
2020-10-08 14:13:18 -0600

    Refactor game test to split up assertions

--- test/Game-test.js
+++ test/Game-test.js
@@ -22,17 +22,31 @@ describe('Game', function() {
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it('should keep track of the round', function() {
+  it('should keep track of the current round round', function() {
     const prototypeQuestions = data.prototypeData;
+
     game.start();
 
     expect(game.currentRound.deck.cards).is.deep.equal(prototypeQuestions);
+    });
+
+  it('should create new card instances in the current round', function() {
+    game.start();
 
     game.currentRound.deck.cards.forEach((card, index) => {
       expect(game.currentRound.deck.cards[index]).to.be.an.instanceof(Card)
     });
+  });
+
+  it('should create a new deck instance in the current round', function() {
+    game.start();
 
     expect(game.currentRound.deck).to.be.an.instanceof(Deck);
+  });
+
+  it('should create a new round instance for the current round', function() {
+    game.start();
+
     expect(game.currentRound).to.be.an.instanceof(Round);
   });
 });
