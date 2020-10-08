bc9f4f17171be8acb63d6b5541bacd1c8290b099
JP Carey <jpcarey4@gmail.com>
2020-10-07 18:24:27 -0600

    expect round instance in game test

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,6 +2,9 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 describe('Game', function() {
   let game;
@@ -18,7 +21,9 @@ describe('Game', function() {
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it('should start the game by creating cards', function() {
+  it('should keep track of the round', function() {
     game.start();
+
+    expect(game.currentRound).to.be.an.instanceof(Round);
   });
 });
