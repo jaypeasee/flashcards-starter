0e5cc6acffa93047a47a0a40a9bb96a256163d79
JP Carey <jpcarey4@gmail.com>
2020-10-08 14:14:50 -0600

    Change all Game test functions use fat arrows

--- test/Game-test.js
+++ test/Game-test.js
@@ -14,15 +14,15 @@ describe('Game', function() {
     game = new Game();
   })
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
 
-  it('should be an instance of Game', function() {
+  it('should be an instance of Game', () => {
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it('should keep track of the current round round', function() {
+  it('should keep track of the current round round', () => {
     const prototypeQuestions = data.prototypeData;
 
     game.start();
@@ -30,7 +30,7 @@ describe('Game', function() {
     expect(game.currentRound.deck.cards).is.deep.equal(prototypeQuestions);
     });
 
-  it('should create new card instances in the current round', function() {
+  it('should create new card instances in the current round', () => {
     game.start();
 
     game.currentRound.deck.cards.forEach((card, index) => {
@@ -38,13 +38,13 @@ describe('Game', function() {
     });
   });
 
-  it('should create a new deck instance in the current round', function() {
+  it('should create a new deck instance in the current round', () => {
     game.start();
 
     expect(game.currentRound.deck).to.be.an.instanceof(Deck);
   });
 
-  it('should create a new round instance for the current round', function() {
+  it('should create a new round instance for the current round', () => {
     game.start();
 
     expect(game.currentRound).to.be.an.instanceof(Round);
