b1d14ffeb7a1c95ceeb66eb323f14f641a1ad5cc
JP Carey <jpcarey4@gmail.com>
2020-10-08 14:22:24 -0600

    Change all functions in Deck test to fat arrows

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
 
-describe('Deck', function() {
+describe('Deck', () => {
   let card1;
   let card2;
   let card3;
@@ -13,7 +13,7 @@ describe('Deck', function() {
   let deck1;
   let deck2;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card1 = new Card(1, 'Who\'s Harry Potter\'s godfather?', ['Dumbledore', 'Sirius', 'Malfoy'], 'Sirius');
 
     card2 = new Card(2, 'What position in Quidditch does Harry Play?', ['seeker', 'beater', 'chaser'], 'seeker');
@@ -29,22 +29,21 @@ describe('Deck', function() {
     deck2 = new Deck([card4, card5]);
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Deck).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
+  it('should be an instance of Deck', () => {
     expect(deck1).to.be.an.instanceof(Deck);
   });
 
-  it('should be able to hold cards', function() {
+  it('should be able to hold cards', () => {
     expect(deck1.cards).to.deep.equal([card1, card2, card3]);
     expect(deck2.cards).to.deep.equal([card4, card5]);
   });
 
-  it('should be able to count its cards', function() {
+  it('should be able to count its cards', () => {
     expect(deck1.countCards()).to.equal(3);
     expect(deck2.countCards()).to.equal(2);
   });
-
 });
